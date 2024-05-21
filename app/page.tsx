57835b53a83c0e24785598ccec5313a5f40bbaa9
TobCraft3521 <tobiashackenberg@outlook.de>
2024-05-21 09:41:25 +0200

    insane new hero section in landing page + app component now has a name : /

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import FullWidthSection from "@/components/landing-page/blabla";
-import Flags from "@/components/landing-page/languages";
-import Flueny from "@/components/header/linguapro-logo";
-import Header from "@/components/header/header";
-import { Button } from "@/components/ui/button";
-import People from "@/public/imgs/people.png";
-import Image from "next/image";
-import Link from "next/link";
-import IntroductionBlock from "@/components/landing-page/intro";
-import AdditionalInfoBlock from "@/components/landing-page/third";
-import { BackgroundBoxesDemo } from "@/components/landing-page/background-boxes";
-import Hero from "@/components/landing-page/hero";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import FullWidthSection from "@/components/landing-page/blabla"
+import Flags from "@/components/landing-page/languages"
+import Flueny from "@/components/header/linguapro-logo"
+import Header from "@/components/header/header"
+import { Button } from "@/components/ui/button"
+import People from "@/public/imgs/people.png"
+import Image from "next/image"
+import Link from "next/link"
+import IntroductionBlock from "@/components/landing-page/intro"
+import AdditionalInfoBlock from "@/components/landing-page/third"
+import { BackgroundBoxesDemo } from "@/components/landing-page/background-boxes"
+import Hero from "@/components/landing-page/hero"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
-export default () => {
+export default function App() {
   return (
     <ScrollArea className="h-screen w-full">
       <Hero />
@@ -20,5 +20,5 @@ export default () => {
       <FullWidthSection />
       <AdditionalInfoBlock />
     </ScrollArea>
-  );
-};
+  )
+}
